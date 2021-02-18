bb60157d7e2dca05c118ba893c43c92b8016fe67
Marvin Raiser <marvin_raiser@web.de>
2021-02-18 18:48:03 +0100

    fixed wrong path to nginx proxy

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -1,2 +1,2 @@
-sudo ln -f frontend/s2quat.raiser.dev.conf ../nginx-proxy/sites/
-sudo ln -f backend/api.raiser.dev.conf ../nginx-proxy/sites/
+sudo ln -f frontend/s2quat.raiser.dev.conf ../proxy/sites/
+sudo ln -f backend/api.raiser.dev.conf ../proxy/sites/
