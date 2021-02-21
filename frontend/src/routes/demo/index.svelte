ea1382556d9df6b5a4d40ae836811f17606d6f35
Marvin Raiser <marvin_raiser@web.de>
2021-02-21 19:28:45 +0100

    Add: ToDos

--- frontend/src/routes/demo/index.svelte
+++ frontend/src/routes/demo/index.svelte
@@ -1,1 +1,21 @@
-demo page goes here
+<script>
+    // Startpage with Info Text and Images
+    // Selection Page
+    // Answerpage
+    // ModelView with clickable fields
+    // SurveyView field specific pages
+    // button to get back to modelView
+    // second layer expandable tab
+    // thirdLayer.children.length === 1 ? thirdLayer.children[0] : thirdLayer.children (as group of fourth dimension)
+    // rating from 1 to 5 (disagree to agree)
+    // Analytics
+    // mean score of field
+    // tbd
+
+    /*
+    Pages
+    - ScenarioPage (Info about the scenario, later with TestPerson creation)
+    - SelectionPage (Select fields or scenarios, later not used)
+    - SurveyPage
+    */
+</script>
