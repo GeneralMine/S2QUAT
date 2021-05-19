f6e0d25012bd64dc73d1a0e614c7c33cfc8c5aa0
Marvin Raiser <marvin_raiser@web.de>
2021-05-19 20:56:28 +0200

    update breadcrumbs

--- nextFrontend/src/lib/Nav/Breadcrumbs.svelte
+++ nextFrontend/src/lib/Nav/Breadcrumbs.svelte
@@ -1,37 +1,17 @@
 <script>
-	import Icon from '$lib/common/Icon.svelte';
-	import BreadcrumbsItem from '$lib/Nav/BreadcrumbsItem.svelte';
+	import BreadcrumbsItem from '$lib/Nav/Breadcrumbs/BreadcrumbsItem.svelte';
+	import BreadcrumbsSeperator from '$lib/Nav/Breadcrumbs/BreadcrumbsSeperator.svelte';
 
 	//TODO: build from breadcrumbs.js store
-
-	// icons
-	// / -> Home
-	// /questions -> Home, Questions
-	// /models -> Home, Models
-	// /companies/ID -> Home, Companies,
-	// /companies/ID/employees -> Home, Companies, Employees
-	let iconNames = [
-		'home',
-		'companies',
-		'employees',
-		'projects',
-		'users',
-		'analytics',
-		'scenaries',
-		'surveyResponses',
-		'testPersons',
-		'models',
-		'questions',
-		'questionAnswers'
-	];
 
 	export let crumbs = [];
 </script>
 
 <div class="breadcrumbsContainer">
-	<BreadcrumbsItem name="Home" is_last={crumbs.length > 0} />
+	<BreadcrumbsItem name="home" is_last={crumbs.length > 0} />
 	{#each crumbs as crumb, i}
-		<BreadcrumbsItem name={crumb} is_last={i >= crumbs.length - 1} />
+		<BreadcrumbsSeperator />
+		<BreadcrumbsItem name={crumb} />
 	{/each}
 </div>
 
@@ -39,5 +19,7 @@
 	.breadcrumbsContainer {
 		display: flex;
 		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
---
+++ nextFrontend/src/lib/Nav/Breadcrumbs/BreadcrumbsItem.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Icon from '$lib/common/Icon.svelte';
+
+	export let name;
+	export let icon_name = name;
+</script>
+
+<div class="breadcrumbsItemContainer">
+	<Icon name={icon_name} />
+	<span>{name}</span>
+</div>
+
+<style>
+	.breadcrumbsItemContainer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 25px;
+		border: 2px solid #707070;
+		flex-direction: row;
+		user-select: none;
+		margin-top: 0.5rem;
+		padding-right: 0.5rem;
+		margin-bottom: 0.5rem;
+	}
+</style>
