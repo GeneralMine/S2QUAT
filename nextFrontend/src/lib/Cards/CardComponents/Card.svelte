4d838d5eed2f816006b87a192d00f3fc5df593fd
Marvin Raiser <marvin_raiser@web.de>
2021-05-19 20:54:29 +0200

    implement cards and companycard

---
+++ nextFrontend/src/lib/Cards/CardComponents/Card.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Surface from '$lib/common/Surface.svelte';
+</script>
+
+<Surface>
+	<div class="cardContainer">
+		<slot />
+	</div>
+</Surface>
+
+<style>
+	.cardContainer {
+		display: flex;
+		flex-direction: column;
+		width: 244px;
+		height: 144px;
+		padding-top: 0.75rem;
+		padding-bottom: 0.75rem;
+		padding-left: 1.5rem;
+		padding-right: 1.5rem;
+		margin: 0;
+	}
+</style>
---
+++ nextFrontend/src/lib/Cards/CardComponents/CardNumber.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let value = 42;
+</script>
+
+<h2 class="number">{value}</h2>
+
+<style>
+	h2 {
+		font-size: 2rem;
+		margin-bottom: 0rem;
+		padding-bottom: 0rem;
+	}
+</style>
