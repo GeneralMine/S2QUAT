4480783a35c13d9a2bed7f067e9909a58fe7f89b
Marvin Raiser <marvin_raiser@web.de>
2021-06-29 09:46:36 +0200

    merge nextFrontend into frontend

--- nextFrontend/src/lib/Nav/Breadcrumbs/BreadcrumbsSeperator.svelte
+++
@@ -1,9 +0,0 @@
-<div class="breadcrumbsSeperatorContainer" />
-
-<style>
-	.breadcrumbsSeperatorContainer {
-		width: 2px;
-		height: 10px;
-		background-color: gray;
-	}
-</style>
--- nextFrontend/src/lib/is_mobile.js
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store";
-
-export const is_mobile = writable(window.innerWidth < 768);
