cecaf364bd5cef2828a3eaf322b57f5f0a1e42ae
Marvin Raiser <marvin_raiser@web.de>
2021-07-06 18:10:56 +0200

    comment the ugly workaround

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 import * as Prisma from '@prisma/client';
-console.log(JSON.stringify(Prisma).substr(0, 0))
+console.log(JSON.stringify(Prisma).substr(0, 0)); // This ugly line is neccessary for forcing the build to execute this locally on startup
 let PrismaClient;
 if (Prisma.PrismaClient) {
     PrismaClient = Prisma.PrismaClient;
@@ -20,10 +20,3 @@ import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 export const prisma = new PrismaClient();
  */
-
-
-/*
-
-
-
-*/
