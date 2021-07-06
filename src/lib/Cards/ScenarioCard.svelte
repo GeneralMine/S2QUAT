5d3bc9bd74031f02207a88efc569da5383be8f8d
Marvin Raiser <marvin_raiser@web.de>
2021-07-07 01:41:49 +0200

    add scenario card

---
+++ src/lib/Cards/ScenarioCard.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Card from './CardComponents/Card.svelte';
+	export let name;
+	export let description;
+</script>
+
+<Card>
+	<div class="column clickArea" on:click>
+		<h2>{name}</h2>
+		<p class="textBlock">{description}</p>
+	</div>
+</Card>
+
+<style>
+	.column {
+		align-items: center;
+		justify-content: center;
+	}
+</style>
