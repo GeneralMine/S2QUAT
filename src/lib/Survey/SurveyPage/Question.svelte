d63c4c562587539fcd520ef3e4a1337f3068e5f6
Marvin Raiser <marvin_raiser@web.de>
2021-07-08 00:38:34 +0200

    fix missing values after page change

--- src/lib/Survey/SurveyPage/Question.svelte
+++ src/lib/Survey/SurveyPage/Question.svelte
@@ -6,47 +6,31 @@
 	import RadioButtonInput from './inputs/RadioButtonInput.svelte';
 
 	export let question;
-	$: console.log(question.id + ':', question);
-	question.typeOptions.push('Random');
-	let score;
-	let text;
-	let boolean;
-	let checkbox;
-	let radiobutton;
 
 	if (!question.answer) {
 		question.answer = {};
-		score = question.answer.score;
-		text = question.answer.text;
-		boolean = question.answer.boolean;
-		checkbox = question.answer.checkbox;
-		radiobutton = question.answer.radiobutton;
-		console.log('Restored stuff:', score);
 	}
-
-	$: question.answer.score = score || question.answer.score;
-	$: question.answer.text = text || question.answer.text;
-	$: question.answer.boolean = boolean || question.answer.boolean;
-	$: question.answer.checkbox = checkbox || question.answer.checkbox;
-	$: question.answer.radiobutton = radiobutton || question.answer.radiobutton;
 </script>
 
 <div class="content">
 	<p class="textBlock">{question.description}</p>
 
 	{#if question.typ === 'SCORE'}
-		<ScoreInput bind:score />
+		<ScoreInput bind:score={question.answer.score} />
 	{:else if question.typ === 'TEXT'}
-		<TextInput bind:text />
+		<TextInput bind:text={question.answer.text} />
 	{:else if question.typ === 'SCORE_TEXT'}
-		<ScoreInput bind:score />
-		<TextInput bind:text />
+		<ScoreInput bind:score={question.answer.score} />
+		<TextInput bind:text={question.answer.text} />
 	{:else if question.typ === 'BOOLEAN'}
-		<BooleanInput bind:boolean options={question.typeOptions} />
+		<BooleanInput bind:boolean={question.answer.boolean} options={question.typeOptions} />
 	{:else if question.typ === 'CHECKBOX'}
-		<CheckBoxInput bind:checkbox options={question.typeOptions} />
+		<CheckBoxInput bind:checkbox={question.answer.checkbox} options={question.typeOptions} />
 	{:else if question.typ === 'RADIOBUTTON'}
-		<RadioButtonInput bind:radiobutton options={question.typeOptions} />
+		<RadioButtonInput
+			bind:radiobutton={question.answer.radiobutton}
+			options={question.typeOptions}
+		/>
 	{/if}
 </div>
 
