6c84b8b51a0ef796266c8feda97947880b9dfb4e
Marvin Raiser <marvin_raiser@web.de>
2021-07-08 16:44:11 +0200

    remove auto versioning, because it wont work

--- src/lib/Common/Footer.svelte
+++ src/lib/Common/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footerContainer">
-	<a class="version" href="https://github.com/GeneralMine/S2QUAT">__APP_VERSION__</a>
+	<a class="version" href="https://github.com/GeneralMine/S2QUAT">v2.0.1</a>
 	<div class="linkContainer">
 		<a href="impressum">Impressum</a>
 		<a href="privacy">Datenschutz</a>
