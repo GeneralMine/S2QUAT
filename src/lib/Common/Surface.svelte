2979f5b71410424c857229e852e5146ec7dac41b
Marvin Raiser <marvin_raiser@web.de>
2021-07-08 20:04:05 +0200

    undo cleanup of surface

--- src/lib/Common/Surface.svelte
+++ src/lib/Common/Surface.svelte
@@ -4,6 +4,8 @@
 	const dispatch = createEventDispatcher();
 
 	export let title;
+	export let edit = false;
+	export let remove = false;
 	export let padding = false;
 	export let clickArea = false;
 	export let smallTitle = false;
@@ -13,6 +15,20 @@
 	<div class="surfaceHeader">
 		{#if title}
 			<h1 class="surfaceHeaderTitle" class:clickArea class:smallTitle on:click>{title}</h1>
+		{/if}
+		{#if edit || remove}
+			<div class="buttons">
+				{#if edit}
+					<div class="edit button clickArea" on:click={() => dispatch('edit', {})}>
+						<Icon name="edit" />
+					</div>
+				{/if}
+				{#if remove}
+					<div class="delete button clickArea" on:click={() => dispatch('remove', {})}>
+						<Icon name="delete" />
+					</div>
+				{/if}
+			</div>
 		{/if}
 		<slot name="header" />
 	</div>
@@ -47,6 +63,21 @@
 		margin-left: 1em;
 		margin-right: 1em;
 		background-color: var(--surface-color);
+	}
+	.buttons {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		padding-right: 2rem;
+		padding-left: 3rem;
+	}
+	.button {
+		margin-right: 1rem;
+		padding: 0.5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 	.padding {
 		padding: 2rem;
