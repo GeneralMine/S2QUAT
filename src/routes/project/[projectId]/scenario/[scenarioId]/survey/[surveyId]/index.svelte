248c786c9a9ee79a1ec03ef5824407f583064872
Marvin Raiser <marvin_raiser@web.de>
2021-07-11 11:49:23 +0200

    add button redirect

--- src/routes/project/[projectId]/scenario/[scenarioId]/survey/[surveyId]/index.svelte
+++ src/routes/project/[projectId]/scenario/[scenarioId]/survey/[surveyId]/index.svelte
@@ -4,16 +4,8 @@
 		try {
 			let [{ project }, { scenario }, { survey }] = await Promise.all([
 				get(`project/${page.params.projectId}/get`, session.token, fetch),
-				get(
-					`project/${page.params.projectId}/scenario/${page.params.scenarioId}/get`,
-					session.token,
-					fetch
-				),
-				get(
-					`project/${page.params.projectId}/scenario/${page.params.scenarioId}/survey/${page.params.surveyId}/get`,
-					session.token,
-					fetch
-				)
+				get(`project/${page.params.projectId}/scenario/${page.params.scenarioId}/get`, session.token, fetch),
+				get(`project/${page.params.projectId}/scenario/${page.params.scenarioId}/survey/${page.params.surveyId}/get`, session.token, fetch)
 			]);
 			return { props: { project, scenario, survey } };
 		} catch (err) {
@@ -37,26 +29,17 @@
 	import PagePrompt from '$lib/Prompt/PagePrompt.svelte';
 	$crumbs = [];
 	if (project.company) {
-		$crumbs = [
-			CrumbBuilder.create(project.company.name, `/company/${project.company.id}`, 'company').build()
-		];
+		$crumbs = [CrumbBuilder.create(project.company.name, `/company/${project.company.id}`, 'company').build()];
 	}
 	$crumbs = [
 		...$crumbs,
 		CrumbBuilder.create(project.name, `/project/${project.id}`, 'project').build(),
-		CrumbBuilder.create(
-			scenario.name,
-			`/project/${project.id}/scenario/${scenario.id}`,
-			'scenario'
-		).build(),
-		CrumbBuilder.create(
-			survey.name,
-			`/project/${project.id}/scenario/${scenario.id}/survey/${survey.id}`,
-			'survey'
-		).build()
+		CrumbBuilder.create(scenario.name, `/project/${project.id}/scenario/${scenario.id}`, 'scenario').build(),
+		CrumbBuilder.create(survey.name, `/project/${project.id}/scenario/${scenario.id}/survey/${survey.id}`, 'survey').build()
 	];
 	/*******************************************/
 	import { del, unpack } from '$lib/utils/api.js';
+	import { goto } from '$app/navigation';
 	let pagePrompt = false;
 	let selectedPage;
 
@@ -76,9 +59,7 @@
 	}
 
 	async function removePage(page) {
-		const { res, err } = await unpack(() =>
-			del(`project/${project}/scenario/${scenario}/survey/${survey}/page/${page.id}`)
-		);
+		const { res, err } = await unpack(() => del(`project/${project}/scenario/${scenario}/survey/${survey}/page/${page.id}`));
 		if (res.id) {
 			for (let index = 0; index < survey.pages.length; index++) {
 				if (survey.pages[index].id === res.id) {
@@ -110,7 +91,7 @@
 
 	<Surface smallTitle={true} title="Der Fragebogen" padding={true} margin={true}>
 		<div slot="header">
-			<button>Fragebogen starten</button>
+			<button on:click={() => goto('survey')}>Fragebogen starten</button>
 		</div>
 		{#each survey.pages as page}
 			<Surface
