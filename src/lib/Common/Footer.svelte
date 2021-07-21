413a8ea7e14d6ad350a284cbe26fd7d426b0f84d
Marvin Raiser <marvin_raiser@web.de>
2021-07-21 09:25:54 +0200

    2.0.8

--- src/lib/Common/Footer.svelte
+++ src/lib/Common/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footerContainer">
-	<a class="version" href="https://github.com/GeneralMine/S2QUAT/releases">v2.0.7</a>
+	<a class="version" href="https://github.com/GeneralMine/S2QUAT/releases">v2.0.8</a>
 	<div class="linkContainer">
 		<a href="impressum">Impressum</a>
 		<a href="privacy">Datenschutz</a>
